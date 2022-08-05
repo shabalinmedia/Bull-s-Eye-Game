d82260046591e3e71620483f5129daf32df75c3e
Alex Shabalin <shabalin.media@gmail.com>
2022-08-05 21:46:27 +0600

    Added working slider.

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue = 50
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,18 +17,28 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert() {
+        
+        let message = "The value of the slider is: \(currentValue)"
+        
         let alert = UIAlertController(
         title: "Hello, World",
-        message: "This is my first app!",
+        message: message,
         preferredStyle: .alert)
         
         let action = UIAlertAction(
-        title: "Awesome",
+        title: "OK",
         style: .default,
         handler: nil)
         
         alert.addAction(action)
+        
         present(alert, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        
+        currentValue = lround(Double(slider.value))
     }
 }
 

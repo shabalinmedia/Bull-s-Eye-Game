1f2ba89aabd199e488f2704b5a3b0e36bea2c268
Alex Shabalin <shabalin.media@gmail.com>
2022-08-05 20:41:24 +0600

    First commit.

---
+++ BullsEye/BullsEye/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Alexander Shabalin on 4/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showAlert() {
+        let alert = UIAlertController(
+        title: "Hello, World",
+        message: "This is my first app!",
+        preferredStyle: .alert)
+        
+        let action = UIAlertAction(
+        title: "Awesome",
+        style: .default,
+        handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+}
+

444a5e1baa017b73ef04ff1d9d3afda6db844a15
Alex Shabalin <shabalin.media@gmail.com>
2022-08-05 23:07:10 +0600

    Added startNewRuond() and updateLabels().

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -10,13 +10,13 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
     var currentValue = 0
     var targetValue = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        currentValue = lround(Double(slider.value))
-        targetValue = Int.random(in: 1...100)
+        startNewRound()
     }
 
     @IBAction func showAlert() {
@@ -36,12 +36,24 @@ class ViewController: UIViewController {
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+        startNewRound()
     }
     
     
     @IBAction func sliderMoved(_ slider: UISlider) {
         
         currentValue = lround(Double(slider.value))
+    }
+    
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
     }
 }
 
